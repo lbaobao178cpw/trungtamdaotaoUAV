85dbdb4e698d3a1b53cc21640eaebbb959347890
Gia Huy <trangiahuy04092018@example.com>
2026-01-24 11:50:00 +0700

    ok la

--- frontend/src/components/course/CoursedetailPage.jsx
+++ frontend/src/components/course/CoursedetailPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './CourseDetailPage.css'; // Đảm bảo import CSS mới
+import './CoursedetailPage.css'; // Đảm bảo import CSS mới
 import noVideoImage from "../assets/noVideoImage.png";
 import { notifySuccess, notifyError, notifyWarning } from '../../lib/notifications';
 import CourseScoreboard from './CourseScoreboard';
